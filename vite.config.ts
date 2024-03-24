86310406c4a7aaaac5326b56a21461abdd07d4a9
Mohamed Hedi Ben Ali <mohamedhedi.benali02@gmail.com>
2024-03-25 00:19:25 +0100

    feat: enable https

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), mkcert()],
+	server: {
+		https: true,
+		proxy: {}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
