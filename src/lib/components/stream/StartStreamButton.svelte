5b342fb2f3e88187f93304b4e9e2bbcd527f9d27
Mohamed Hedi Ben Ali <mohamedhedi.benali02@gmail.com>
2024-03-29 03:21:16 +0100

    feat: add video capture
    Co-authored-by: Zeineb Benabdallah <88910661+zei-ZZ@users.noreply.github.com>

---
+++ src/lib/components/stream/StartStreamButton.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { mediaStream } from '$lib/stores/stream';
+
+	const displayMediaOptions: DisplayMediaStreamOptions = {
+		video: true,
+		audio: true
+	};
+
+	async function startCapture(displayMediaOptions: DisplayMediaStreamOptions) {
+		let captureStream = null;
+
+		try {
+			captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+		} catch (err) {
+			console.error(`Error: ${err}`);
+		}
+		return captureStream;
+	}
+
+	async function handleClick(_event: Event) {
+		$mediaStream = await startCapture(displayMediaOptions);
+	}
+</script>
+
+<button on:click={handleClick}>Start Stream</button>
