95afb032521408b92bbd11131ef31f7ee8f20394
Mohamed Hedi Ben Ali <mohamedhedi.benali02@gmail.com>
2024-03-31 18:35:40 +0100

    feat: add live_chat_service app

---
+++ apps/live_chat_service/config/config.exs
@@ -0,0 +1,34 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+import Config
+
+config :live_chat_service,
+  generators: [timestamp_type: :utc_datetime]
+
+# Configures the endpoint
+config :live_chat_service, LiveChatServiceWeb.Endpoint,
+  url: [host: "localhost"],
+  adapter: Bandit.PhoenixAdapter,
+  render_errors: [
+    formats: [json: LiveChatServiceWeb.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: LiveChatService.PubSub,
+  live_view: [signing_salt: "ImBkmOYc"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
