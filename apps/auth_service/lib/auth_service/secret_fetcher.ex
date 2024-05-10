b89dc547e36bd204f472e2175a78460c0bb29785
zei-ZZ <zeineb.benabdallah@insat.ucar.tn>
2024-05-10 21:46:33 +0100

    fix: change fetching from priv

--- apps/auth_service/lib/auth_service/secret_fetcher.ex
+++ apps/auth_service/lib/auth_service/secret_fetcher.ex
@@ -1,25 +1,23 @@
 defmodule AuthService.SecretFetcher do
   def fetch_signing_secret(_module, _opts) do
     secret =
-      "./priv/keys/secret_key.pem"
+      "keys/secret_key.pem"
       |> fetch()
 
     {:ok, secret}
   end
 
-  @spec fetch_verifying_secret(any(), any(), any()) :: {:ok, list() | JOSE.JWK.t()}
   def fetch_verifying_secret(_module, _headers, _opts) do
     secret =
-      "./priv/keys/public_key.pem"
+      "keys/public_key.pem"
       |> fetch()
 
     {:ok, secret}
   end
 
   defp fetch(relative_path) do
-    # :code.priv_dir(:debug_guardian)
-    # |> Path.join(relative_path)
-    relative_path
+    :code.priv_dir(:auth_service)
+    |> Path.join(relative_path)
     |> JOSE.JWK.from_pem_file()
   end
 end
