cfc99643d61fb935d8ec5f1a93e38c357250f081
Mohamed Hedi Ben Ali <94202725+medhedibenali@users.noreply.github.com>
2024-05-31 05:08:38 +0100

    style: format the project

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -11,10 +11,10 @@
 			background-size: cover;"
 	class="h-full w-full"
 >
-	<div class="flex h-full items-center w-full">
+	<div class="flex h-full w-full items-center">
 		<div
 			style="height: 60%; width:50%"
-			class="relative mx-auto flex  flex-row justify-around items-center rounded-lg border border-black/[0.2] bg-black bg-opacity-50 p-4 dark:border-white/[0.2]"
+			class="relative mx-auto flex flex-row items-center justify-around rounded-lg border border-black/[0.2] bg-black bg-opacity-50 p-4 dark:border-white/[0.2]"
 		>
 			<div class="relative z-10 mb-5 text-center text-lg font-bold md:text-2xl">Welcome Back !</div>
 			<LoginForm data={data.form} />
