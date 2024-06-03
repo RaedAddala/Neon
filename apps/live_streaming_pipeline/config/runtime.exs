0e1e1f609d9f260a193af2cba315e63fbfa0ecdb
Mohamed Hedi Ben Ali <94202725+medhedibenali@users.noreply.github.com>
2024-06-03 23:32:58 +0100

    ci: add Dockerfile

--- apps/live_streaming_pipeline/config/runtime.exs
+++ apps/live_streaming_pipeline/config/runtime.exs
@@ -6,6 +6,20 @@ import Config
 # and secrets from environment variables or elsewhere. Do not define
 # any compile-time configuration in here, as it won't be applied.
 # The block below contains prod specific runtime configuration.
+
+# ## Using releases
+#
+# If you use `mix release`, you need to explicitly enable the server
+# by passing the PHX_SERVER=true when you start it:
+#
+#     PHX_SERVER=true bin/live_streaming_pipeline start
+#
+# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
+# script that automatically sets the env var above.
+if System.get_env("PHX_SERVER") do
+  config :live_streaming_pipeline, LiveStreamingPipelineWeb.Endpoint, server: true
+end
+
 if config_env() == :prod do
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
@@ -19,7 +33,13 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
+  host = System.get_env("PHX_HOST") || "example.com"
+  port = String.to_integer(System.get_env("PORT") || "4000")
+
+  config :live_streaming_pipeline, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
+
   config :live_streaming_pipeline, LiveStreamingPipelineWeb.Endpoint,
+    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
